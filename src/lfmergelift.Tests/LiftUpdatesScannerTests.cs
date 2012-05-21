a1a2849ff8f04a4a0207733b9a61e469ea8a4cd4
Rick MacLean <rick_maclean@sil.org>
2012-05-21 13:48:17 +0700

    Parse lift.update file names, find files with wrong filename format, get projectNames to apply updates too.

--- src/lfmergelift.Tests/LiftUpdatesScannerTests.cs
+++ src/lfmergelift.Tests/LiftUpdatesScannerTests.cs
@@ -1,10 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading;
 using NUnit.Framework;
 using Palaso.Lift.Merging;
 using Palaso.TestUtilities;
+
 
 namespace lfmergelift.Tests
 {
@@ -23,14 +25,14 @@ namespace lfmergelift.Tests
 			}
 
 
-			public string LiftUpdatesPath()
+			public string LiftUpdatesPath
 			{
-				return System.IO.Path.Combine(Path, _updatesFolder);
+				get { return System.IO.Path.Combine(Path, _updatesFolder); }
 			}
 
 			public void CreateUpdateFolder()
 			{
-				Directory.CreateDirectory(LiftUpdatesPath());
+				Directory.CreateDirectory(LiftUpdatesPath);
 			}
 
 			public void Dispose()
@@ -42,93 +44,10 @@ namespace lfmergelift.Tests
 
 		private const string _baseLiftFileName = "base.lift";
 
-		static private readonly string[] s_LiftMainFile = new[]
-		{
-			"<?xml version=\"1.0\" encoding=\"UTF-8\"?>",
-			"<lift producer=\"SIL.FLEx 7.3.0.41038\" version=\"0.13\">",
-			"<header>",
-			"<ranges>",
-			"<range id=\"dialect\" href=\"file://C:/Users/maclean/Documents/aa Work/LIFToutput/LIFToutput.lift-ranges\"/>",
-			"</ranges>",
-			"<fields>",
-			"<field tag=\"summary-definition\">",
-			"<form lang=\"en\"><text>A summary definition (located at the entry level in the Entry pane) is a general definition summarizing all the senses of a primary entry. It has no theoretical value; its use is solely pragmatic.</text></form>",
-			"</field>",
-
-			"<field tag=\"scientific-name\">",
-			"<form lang=\"en\"><text>This field stores the scientific name pertinent to the current sense.</text></form>",
-			"</field>",
-			"</fields>",
-			"</header>",
-
-			//entry 1
-			"<entry dateCreated=\"2011-03-01T18:09:46Z\" dateModified=\"2011-03-01T18:30:07Z\" guid=\"ecfbe958-36a1-4b82-bb69-ca5210355400\" id=\"hombre_ecfbe958-36a1-4b82-bb69-ca5210355400\">",
-			"<lexical-unit>",
-			"<form lang=\"es\"><text>hombre</text></form>",
-			"<form lang=\"fr-Zxxx-x-AUDIO\"><text>hombre634407358826681759.wav</text></form>",
-			"<form lang=\"Fr-Tech 30Oct\"><text>form in bad WS</text></form>",
-			"</lexical-unit>",
-			"<trait name=\"morph-type\" value=\"root\"></trait>",
-			"<pronunciation>",
-			"<form lang=\"fr\"><text>ombre</text></form>",
-			"<media href=\"Sleep Away.mp3\">",
-			"</media>",
-			"</pronunciation>",
-			"<sense id=\"hombre_f63f1ccf-3d50-417e-8024-035d999d48bc\">",
-			"<grammatical-info value=\"Noun\">",
-			"</grammatical-info>",
-			"<gloss lang=\"en\"><text>man</text></gloss>",
-			"<definition>",
-			"<form lang=\"en\"><text>male adult human <span href=\"file://others/SomeFile.txt\" class=\"Hyperlink\">link</span></text></form>",
-			"<form lang=\"fr-Zxxx-x-AUDIO\"><text>male adult634407358826681760.wav</text></form>",
-			"</definition>",
-			"<illustration href=\"Desert.jpg\">",
-			"<label>",
-			"<form lang=\"fr\"><text>Desert</text></form>",
-			"</label>",
-			"</illustration>",
-			"<illustration href=\"subfolder/MyPic.jpg\">",
-			"<label>",
-			"<form lang=\"fr\"><text>My picture</text></form>",
-			"</label>",
-			"</illustration>",
-			"<trait name=\"semantic-domain-ddp4\" value=\"2.6.5.1 Man\"></trait>",
-			"<trait name=\"semantic-domain-ddp4\" value=\"2.6.4.4 Adult\"></trait>",
-			"</sense>",
-			"<sense id=\"creature7ddb62da-fa55-404f-b944-46b71b00c8c8\">",
-			"<grammatical-info value=\"Noun\">",
-			"</grammatical-info>",
-			"<gloss lang=\"en\"><text>swimming creature</text></gloss>",
-			"<relation type=\"Part\" ref=\"a3f48811-e5e4-43d0-9ce3-dcd6af3ee07d\"/>",
-			"</sense>",
-			"</entry>",
-
-			//entry 2
-			"<entry dateCreated=\"2012-05-09T06:53:05Z\" dateModified=\"2012-05-09T06:53:53Z\" id=\"leg_d6b29be3-a278-4c5f-9c43-2de7cc820e4f\" guid=\"d6b29be3-a278-4c5f-9c43-2de7cc820e4f\">",
-			"<lexical-unit>",
-			"<form lang=\"fr\"><text>leg</text></form>",
-			"</lexical-unit>",
-			"<trait  name=\"morph-type\" value=\"stem\"/>",
-			"<sense id=\"9a3b501a-b487-47c1-b77b-41975c7147d2\">",
-			"<grammatical-info value=\"Noun\">",
-			"</grammatical-info>",
-			"<gloss lang=\"en\"><text>leg</text></gloss>",
-			"<definition>",
-			"<form lang=\"en\"><text>Part of chair or table or animal</text></form>",
-			"</definition>",
-			"<relation type=\"Whole\" ref=\"d5fd85b6-24cf-4a20-9156-9b1ee3959714\"/>",
-			"<relation type=\"Whole\" ref=\"9aaf4b46-f2b5-452f-981f-8517e64e6dc2\"/>",
-			"</sense>",
-			"</entry>",
-
-			"</lift>"
-		};
-
 		private static readonly string s_LiftData1 =
 		   "<entry id='one' guid='0ae89610-fc01-4bfd-a0d6-1125b7281dd1'></entry>"
 		   + "<entry id='two' guid='0ae89610-fc01-4bfd-a0d6-1125b7281d22'><lexical-unit><form lang='nan'><text>test</text></form></lexical-unit></entry>"
 		   + "<entry id='three' guid='80677C8E-9641-486e-ADA1-9D20ED2F5B69'></entry>";
-
 
 		private static readonly string s_LiftUpdate1 =
 			"<entry id='four' guid='6216074D-AD4F-4dae-BE5F-8E5E748EF68A'></entry>"
@@ -140,27 +59,137 @@ namespace lfmergelift.Tests
 			+ "<entry id='six' guid='107136D0-5108-4b6b-9846-8590F28937E8'></entry>";
 
 
+		[Test]
+		public void FindLiftUpdateFiles_TwoLiftUpdateFiles_OneExtraFile()
+		{
+			//This test puts 3 files in the mergerWork/liftUpdates folder.  Two are .lift.updates files and one is not.
+			//Verify that only the .lift.updates files are returned from the call to GetPendingUpdateFiles
+			using (var e = new TestEnvironment())
+			{
+				e.CreateUpdateFolder();
+
+				////Create a file that is not a .lift.update file
+				LfSynchronicMergerTests.WriteFile(_baseLiftFileName, s_LiftData1, e.LiftUpdatesPath);
+				//Create a .lift.update file.
+				LfSynchronicMergerTests.WriteFile("Proj_Sha_extraB" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate1, e.LiftUpdatesPath);
+				//Create another .lift.update file
+				LfSynchronicMergerTests.WriteFile("Proj_Sha_extraA" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate2, e.LiftUpdatesPath);
+				FileInfo[] files = LiftUpdatesScanner.GetPendingUpdateFiles(e.LiftUpdatesPath);
+				Assert.That(files.Length, Is.EqualTo(2));
+				Assert.That(files[0].Name, Is.EqualTo("Proj_Sha_extraA" + SynchronicMerger.ExtensionOfIncrementalFiles));
+				Assert.That(files[1].Name, Is.EqualTo("Proj_Sha_extraB" + SynchronicMerger.ExtensionOfIncrementalFiles));
+
+				var updatesScanner = new LiftUpdatesScanner(e.LiftUpdatesPath);
+				Assert.That(updatesScanner.LiftUpdateFiles.Length, Is.EqualTo(2));
+				Assert.That(updatesScanner.LiftUpdateFiles[0].Name, Is.EqualTo("Proj_Sha_extraA" + SynchronicMerger.ExtensionOfIncrementalFiles));
+				Assert.That(updatesScanner.LiftUpdateFiles[1].Name, Is.EqualTo("Proj_Sha_extraB" + SynchronicMerger.ExtensionOfIncrementalFiles));
+			}
+		}
 
 		[Test]
-		public void FindProjectFolders_OneProject_TwoUpdateFolders_OneWithTwoLiftUpdateFiles()
+		public void FindLiftUpdateFiles_AndVerifyCorrect_UpdateInfo()
+		{
+			//This test gets a number of .lift.update files and verifies that the UpdateInfo structure was correctly built
+			//Each .lift.update file will have the following format.
+			// projX_sha#_timeStamp.lift.update
+			using (var e = new TestEnvironment())
+			{
+				e.CreateUpdateFolder();
+
+				//Create a .lift.update file
+				LfSynchronicMergerTests.WriteFile("ProjA_sha0123_uniqueExtra1" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate1, e.LiftUpdatesPath);
+				//Create a second .lift.update file.
+				LfSynchronicMergerTests.WriteFile("ProjA_sha0123_uniqueExtra2" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate2, e.LiftUpdatesPath);
+				//Create a third .lift.update file.
+				LfSynchronicMergerTests.WriteFile("ProjB_sha3456_time11" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate2, e.LiftUpdatesPath);
+
+				var updatesScanner = new LiftUpdatesScanner(e.LiftUpdatesPath);
+				var liftUpdateFilesInfo = updatesScanner.LiftUpdateFilesInfo;
+				Assert.IsNotNull(liftUpdateFilesInfo);
+				var updateInfoToCheck = from updateInfo in liftUpdateFilesInfo
+											   where updateInfo.Project == "ProjA"
+											   select updateInfo;
+				Assert.That(updateInfoToCheck.Count(), Is.EqualTo(2));
+				VerifyUpdateInfoRecord(updateInfoToCheck.ElementAt(0), "ProjA", "sha0123", "ProjA_sha0123_uniqueExtra1", e.LiftUpdatesPath);
+				VerifyUpdateInfoRecord(updateInfoToCheck.ElementAt(1), "ProjA", "sha0123", "ProjA_sha0123_uniqueExtra2", e.LiftUpdatesPath);
+
+				updateInfoToCheck = from updateInfo in liftUpdateFilesInfo
+										where updateInfo.Project == "ProjB"
+										select updateInfo;
+				Assert.That(updateInfoToCheck.Count(), Is.EqualTo(1));
+				VerifyUpdateInfoRecord(updateInfoToCheck.ElementAt(0), "ProjB", "sha3456", "ProjB_sha3456_time11", e.LiftUpdatesPath);
+			}
+		}
+
+		void VerifyUpdateInfoRecord(UpdateInfo updateInfoRecord, String proj, String sha, String filename, String liftUpdatesPath)
+		{
+			Assert.That(updateInfoRecord.Project, Is.EqualTo(proj));
+			Assert.That(updateInfoRecord.Sha, Is.EqualTo(sha));
+			Assert.That(updateInfoRecord.UpdateFilePath, Is.EqualTo(Path.Combine(liftUpdatesPath, filename + SynchronicMerger.ExtensionOfIncrementalFiles)));
+		}
+
+		[Test]
+		public void FindAllProjectNamesThatHaveLiftUpdates()
 		{
 			using (var e = new TestEnvironment())
 			{
 				e.CreateUpdateFolder();
 
-				////Create a LIFT file with 3 entries which will have updates applied to it.
-				LfSynchronicMergerTests.WriteFile(_baseLiftFileName, s_LiftData1, e.LiftUpdatesPath());
-				//Create a .lift.update file with three entries.  One to replace the second entry in the original LIFT file.
-				//The other two are new and should be appended to the original LIFT file.
-				LfSynchronicMergerTests.WriteFile("LiftChangeFileB" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate1, e.LiftUpdatesPath());
-				//Create a .lift.update file with two entries.  One to replace one of the changes from the first LiftUpdate file and one new entry.
-				LfSynchronicMergerTests.WriteFile("LiftChangeFileA" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate2, e.LiftUpdatesPath());
-				FileInfo[] files = LiftUpdatesScanner.GetPendingUpdateFiles(e.LiftUpdatesPath());
-				Assert.That(files.Length, Is.EqualTo(2));
-				Assert.That(files[0].Name, Is.EqualTo("LiftChangeFileA" + SynchronicMerger.ExtensionOfIncrementalFiles));
-				Assert.That(files[1].Name, Is.EqualTo("LiftChangeFileB" + SynchronicMerger.ExtensionOfIncrementalFiles));
+				//Create a .lift.update file
+				LfSynchronicMergerTests.WriteFile("ProjA_sha0123_uniqueExtra4578" + SynchronicMerger.ExtensionOfIncrementalFiles,
+												  s_LiftUpdate1, e.LiftUpdatesPath);
+				//Create a second .lift.update file.
+				LfSynchronicMergerTests.WriteFile("ProjA_sha0123_uniqueExtra2459" + SynchronicMerger.ExtensionOfIncrementalFiles,
+												  s_LiftUpdate2, e.LiftUpdatesPath);
+				//Create a third .lift.update file.
+				LfSynchronicMergerTests.WriteFile("ProjB_sha3456_time11" + SynchronicMerger.ExtensionOfIncrementalFiles,
+												  s_LiftUpdate2, e.LiftUpdatesPath);
+				//Create a fourth .lift.update file.
+				LfSynchronicMergerTests.WriteFile("ProjK_sha45874563_time114587" + SynchronicMerger.ExtensionOfIncrementalFiles,
+												  s_LiftUpdate2, e.LiftUpdatesPath);
+				//Create a fourth .lift.update file.
+				LfSynchronicMergerTests.WriteFile("ProjC_sha45863_time587" + SynchronicMerger.ExtensionOfIncrementalFiles,
+												  s_LiftUpdate2, e.LiftUpdatesPath);
+
+				var updatesScanner = new LiftUpdatesScanner(e.LiftUpdatesPath);
+				var projectNames = updatesScanner.GetProjectsNamesToUpdate();
+				Assert.IsNotNull(projectNames);
+				Assert.That(projectNames.Count(), Is.EqualTo(4));
+				Assert.That(projectNames.ElementAt(0), Is.EqualTo("ProjA"));
+				Assert.That(projectNames.ElementAt(1), Is.EqualTo("ProjB"));
+				Assert.That(projectNames.ElementAt(2), Is.EqualTo("ProjC"));
+				Assert.That(projectNames.ElementAt(3), Is.EqualTo("ProjK"));
 			}
 		}
 
+		[Test]
+		public void FindAllLiftUpdateFilesWithWrongNameFormat()
+		{
+			using (var e = new TestEnvironment())
+			{
+				e.CreateUpdateFolder();
+
+				//Create a .lift.update file
+				LfSynchronicMergerTests.WriteFile("ProjA_sha0123_uniqueExtra4578" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate1, e.LiftUpdatesPath);
+				LfSynchronicMergerTests.WriteFile("BlahBlah_sha45863time587" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate2, e.LiftUpdatesPath);
+				LfSynchronicMergerTests.WriteFile("ProjC_sha45863_time587" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate2, e.LiftUpdatesPath);
+				LfSynchronicMergerTests.WriteFile("BlahBlahsha45863time" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate2, e.LiftUpdatesPath);
+				LfSynchronicMergerTests.WriteFile("ProjC_sha45_four_partsToName" + SynchronicMerger.ExtensionOfIncrementalFiles, s_LiftUpdate2, e.LiftUpdatesPath);
+
+				var updatesScanner = new LiftUpdatesScanner(e.LiftUpdatesPath);
+				var badLiftUpdateFiles = updatesScanner.UpdateFilesWithWrongNameFormat;
+				Assert.IsNotNull(badLiftUpdateFiles);
+				Assert.That(badLiftUpdateFiles.Count(), Is.EqualTo(2));
+				Assert.That(badLiftUpdateFiles.ElementAt(1), Is.EqualTo(Path.Combine(e.LiftUpdatesPath, "BlahBlah_sha45863time587" + SynchronicMerger.ExtensionOfIncrementalFiles)));
+				Assert.That(badLiftUpdateFiles.ElementAt(0), Is.EqualTo(Path.Combine(e.LiftUpdatesPath, "BlahBlahsha45863time" + SynchronicMerger.ExtensionOfIncrementalFiles)));
+
+				var liftUpdateFilesInfo = updatesScanner.LiftUpdateFilesInfo;
+				Assert.IsNotNull(liftUpdateFilesInfo);
+				Assert.That(liftUpdateFilesInfo.Count(), Is.EqualTo(3));
+				VerifyUpdateInfoRecord(liftUpdateFilesInfo.ElementAt(0), "ProjA", "sha0123", "ProjA_sha0123_uniqueExtra4578", e.LiftUpdatesPath);
+				VerifyUpdateInfoRecord(liftUpdateFilesInfo.ElementAt(1), "ProjC", "sha45863", "ProjC_sha45863_time587", e.LiftUpdatesPath);
+				VerifyUpdateInfoRecord(liftUpdateFilesInfo.ElementAt(2), "ProjC", "sha45", "ProjC_sha45_four_partsToName", e.LiftUpdatesPath);
+			}
+		}
 	}
 }
--- src/lfmergelift/LiftUpdatesScanner.cs
+++ src/lfmergelift/LiftUpdatesScanner.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
 using Chorus.VcsDrivers.Mercurial;
@@ -17,14 +18,96 @@ namespace lfmergelift
 	internal class LiftUpdatesScanner
 	{
 		private HgRepository _hgRepo;
-		private IEnumerable<UpdateInfo> _updateFilesInfo;
+		private List<UpdateInfo> _updateFilesInfo =new List<UpdateInfo>();
 		private String _updatesDirectory;
 		private FileInfo[] _liftUpdateFiles;
+		private String[] _fileNameParts;
+		private List<String> _updateFilesWithWrongNameFormat = new List<String>();
 
 		public LiftUpdatesScanner(String updatesDirectory)
 		{
 			_updatesDirectory = updatesDirectory;
 			_liftUpdateFiles = GetPendingUpdateFiles(_updatesDirectory);
+
+			//????  not sure what I should do if there are no files.
+			if (_liftUpdateFiles.Length < 1)
+			{
+				return;
+			}
+
+			//We will need to sort files by time stamp but maybe not here
+			//Array.Sort(_liftUpdateFiles, new FileInfoLastWriteTimeComparer());
+			int count = _liftUpdateFiles.Length;
+			GetUpdateInfoForEachLiftUpdateFile();
+		}
+
+
+		//Get All Project Names that need updating based on the .lift.update files currently in the liftUpdates folder
+		//sorted alphabetically
+		public IEnumerable<String> GetProjectsNamesToUpdate()
+		{
+			var projectNames = new List<String>();
+			foreach (var fileInfoRecord in _updateFilesInfo)
+			{
+				if (!projectNames.Contains(fileInfoRecord.Project))
+				{
+					projectNames.Add(fileInfoRecord.Project);
+				}
+			}
+			projectNames.Sort();
+			return projectNames;
+		}
+
+		//Get All Sha's for a particular Project based on the .lift.update files currently in the liftUpdates folder
+
+
+		//Get the .lift.update files for a particular project name and sha. Sort by file timeStamp
+
+
+		private void GetUpdateInfoForEachLiftUpdateFile()
+		{
+			foreach (FileInfo file in _liftUpdateFiles)
+			{
+				var filePath = file.FullName;
+				var fileName = file.Name;
+				//verify fileName has correct format  ProjX_Sha#_LastPart
+				//if not then do something useful with the file
+				//if ok then
+				//   get project name
+				//   get sha part
+				//   save full file path
+				//   save fileInfo so that can be used to sort the order to process files.
+				_fileNameParts = fileName.Split(new char[] { '_' }, StringSplitOptions.RemoveEmptyEntries);
+				if (_fileNameParts.Length < 3)
+				{
+					//fileName does not have at least 3 parts so is in the wrong format. Assumption that last part can also contain underscores so this
+					//number can be greater than 3
+					//Do something with this file to report an error.
+					//Debug.Assert(_fileNameParts.Length >= 3, String.Format("Lift update files should must have the following format ProjX_Sha#_timeStamp: file found with name {0}", fileName));
+
+					if (!_updateFilesWithWrongNameFormat.Contains(file.FullName))
+						_updateFilesWithWrongNameFormat.Add(file.FullName);
+				}
+				else
+				{
+					var updateFileInfo = new UpdateInfo();
+					updateFileInfo.Project = GetProjectName(_fileNameParts);
+					updateFileInfo.Sha = GetSha(_fileNameParts);
+					updateFileInfo.UpdateFilePath = filePath;
+					updateFileInfo.SystemFileInfo = file;
+					_updateFilesInfo.Add(updateFileInfo);
+				}
+			}
+		}
+
+		private static string GetSha(String[] fileNameParts)
+		{
+			return fileNameParts[1];
+		}
+
+		private static string GetProjectName(String[] fileNameParts)
+		{
+			return fileNameParts[0];
 		}
 
 		public FileInfo[] LiftUpdateFiles
@@ -32,9 +115,18 @@ namespace lfmergelift
 			get { return _liftUpdateFiles; }
 		}
 
+		public IEnumerable<UpdateInfo> LiftUpdateFilesInfo
+		{
+			get { return _updateFilesInfo; }
+		}
+
+		public List<string> UpdateFilesWithWrongNameFormat
+		{
+			get { return _updateFilesWithWrongNameFormat; }
+		}
+
 		public static FileInfo[] GetPendingUpdateFiles(string pathToLiftUpdates)
 		{
-			//see ws-1035
 			if (!pathToLiftUpdates.Contains(Path.DirectorySeparatorChar.ToString()))
 			{
 				throw new ArgumentException("pathToLiftUpdates must be a full path. Path was " + pathToLiftUpdates);
@@ -44,12 +136,30 @@ namespace lfmergelift
 			// ReSharper restore AssignNullToNotNullAttribute
 			return di.GetFiles("*" + SynchronicMerger.ExtensionOfIncrementalFiles, SearchOption.TopDirectoryOnly);
 		}
+
+
+
 	}
 
 	class UpdateInfo
 	{
-		string Project { get; set; }
-		string Sha { get; set; }
-		string UpdateFilePath { get; set; }
+		public string Project { get; set; }
+		public string Sha { get; set; }
+		public string UpdateFilePath { get; set; }
+		public FileInfo SystemFileInfo { get; set; }
+	}
+
+	internal class FileInfoLastWriteTimeComparer : IComparer<FileInfo>
+	{
+		public int Compare(FileInfo x, FileInfo y)
+		{
+			int timecomparison = DateTime.Compare(x.LastWriteTimeUtc, y.LastWriteTimeUtc);
+			if (timecomparison == 0)
+			{
+				// if timestamps are the same, then sort by name
+				return StringComparer.OrdinalIgnoreCase.Compare(x.FullName, y.FullName);
+			}
+			return timecomparison;
+		}
 	}
 }
