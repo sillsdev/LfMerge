15a960dcfe8da6e8b14cc1fd8647f453ca21fe69
Eberhard Beilharz <eb1@sil.org>
2015-11-06 17:24:37 +0100

    Use scriptureforge database to get project data

--- src/LfMerge/LanguageDepotProject.cs
+++ src/LfMerge/LanguageDepotProject.cs
@@ -13,11 +13,12 @@ namespace LfMerge
 		public LanguageDepotProject(string lfProjectCode)
 		{
 			var client = new MongoClient("mongodb://" + LfMergeSettings.Current.MongoDbHostNameAndPort);
-			var database = client.GetDatabase("languageforge");
-			var collection = database.GetCollection<BsonDocument>("projects");
+			var database = client.GetDatabase("scriptureforge");
+			var projectCollection = database.GetCollection<BsonDocument>("projects");
+			//var userCollection = database.GetCollection<BsonDocument>("users");
 
-			var filter = new BsonDocument("projectCode", lfProjectCode);
-			var list = collection.Find(filter).ToListAsync();
+			var projectFilter = new BsonDocument("projectCode", lfProjectCode);
+			var list = projectCollection.Find(projectFilter).ToListAsync();
 			list.Wait();
 
 			var project = list.Result.FirstOrDefault();
@@ -25,12 +26,14 @@ namespace LfMerge
 				throw new ArgumentException("Can't find project code", "lfProjectCode");
 
 			BsonValue value;
+			if (project.TryGetValue("ldProjectCode", out value))
+				ProjectCode = value.AsString;
+			// TODO: need to get S/R server (language depot public, language depot private, custom, etc).
+			// TODO: ldUsername and ldPassword should come from the users collection
 			if (project.TryGetValue("ldUsername", out value))
 				Username = value.AsString;
 			if (project.TryGetValue("ldPassword", out value))
 				Password = value.AsString;
-			if (project.TryGetValue("ldProjectCode", out value))
-				ProjectCode = value.AsString;
 		}
 
 		public string Username { get; private set; }
