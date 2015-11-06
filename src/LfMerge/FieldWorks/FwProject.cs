6f0caf51d05b534e9d1cc4eda748e2acd563a35b
Robin Munn <robin_munn@sil.org>
2015-11-06 11:21:04 +0700

    Remove old, now-unused testing code from FwProject

--- src/LfMerge/FieldWorks/FwProject.cs
+++ src/LfMerge/FieldWorks/FwProject.cs
@@ -109,69 +109,6 @@ namespace LfMerge.FieldWorks
 
 			return fdoCache;
 		}
-
-		private void DelMe() {
-			Console.WriteLine("Starting UpdateFdoFromMongoDb");
-			// TODO: Get connection string from config, not hardcoded
-			string HardcodedMongoConnectionString = "mongodb://languageforge.local/scriptureforge";
-			string HardcodedProjectCode = "thai_food";
-			string HardcodedDatabasePrefix = "sf_"; // TODO: Is there a better way to get the prefix?
-			//string DatabaseName = "sf_" + HardcodedProjectCode;
-			var client = new MongoClient(HardcodedMongoConnectionString);
-			var sf = client.GetDatabase("scriptureforge");
-			var projects = sf.GetCollection<BsonDocument>("projects");
-			var F = Builders<BsonDocument>.Filter; // Make the next line shorter to read
-			var filter = F.Eq("appName", "lexicon") & F.Eq("projectCode", HardcodedProjectCode);
-			var project = projects.Find(filter).Limit(1).FirstOrDefaultAsync().Result; // Filter on server
-			//var project = projects.Find(doc => doc["appName"] == "lexicon" && doc["projectCode"] == HardcodedProjectCode)
-			//	.Limit(1).ToListAsync().Result.FirstOrDefault(); // Filter on client
-			if (project != null)
-			{
-				Console.WriteLine("Project \"{0}\" has code {1}", project["projectName"], project["projectCode"]);
-				var config = project["config"];
-				var fields = config["entry"]["fieldOrder"] as BsonArray;
-				foreach (var field in fields)
-					Console.WriteLine("  and has field: {0}", field);
-				string dbName = HardcodedDatabasePrefix + project["projectCode"];
-				var db = client.GetDatabase(dbName);
-				var collection = db.GetCollection<BsonDocument>("lexicon");
-				var bar = collection.Find(_ => true).ToListAsync().Result; // Without the Result, we would have to use await
-				foreach (var item in bar)
-				{
-					// To do this properly, we should get the inputSystems from the config["entry"]["fields"]["lexeme"] list
-					// For now, we hardcode the two th-fonipa and th writing system names
-					string th_fonipa;
-					try {
-						th_fonipa = item["lexeme"]["th-fonipa"]["value"].AsString;
-						Console.WriteLine("Item: {0}", th_fonipa);
-					}
-					catch(KeyNotFoundException) {
-						Console.WriteLine("No fonipa for {0}", item["lexeme"]["th"]["value"]);
-					}
-				}
-
-			}
-			Console.WriteLine("Stopping UpdateFdoFromMongoDb");
-
-			return;
-		}
-
-		private static IEnumerable<string> GetListOfMongoDatabases(MongoClient client)
-		{
-			IAsyncCursor<BsonDocument> foo = client.ListDatabasesAsync().Result;
-			List<BsonDocument> l = foo.ToListAsync().Result;
-			IEnumerable<string> result = l.Select(doc => doc["name"].AsString);
-			return result;
-		}
-
-		private static IEnumerable<string> GetListOfCollections(IMongoDatabase db)
-		{
-			IAsyncCursor<BsonDocument> foo = db.ListCollectionsAsync().Result;
-			List<BsonDocument> l = foo.ToListAsync().Result;
-			IEnumerable<string> result = l.Select(doc => doc["name"].AsString);
-			return result;
-		}
-
 	}
 }
 
