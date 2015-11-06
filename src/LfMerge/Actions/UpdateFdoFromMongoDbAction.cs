e8a2c993a56246a8be51f0ceb12b34114042eb28
Robin Munn <robin_munn@sil.org>
2015-11-06 11:17:09 +0700

    More testing code in UpdateFdoFromMongoDbAction
    First proof-of-concept on accessing data from FDO. Updating is next.

--- src/LfMerge/Actions/UpdateFdoFromMongoDbAction.cs
+++ src/LfMerge/Actions/UpdateFdoFromMongoDbAction.cs
@@ -7,6 +7,7 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 using LfMerge.LanguageForge.Config;
 using LfMerge.LanguageForge.Model;
+using SIL.FieldWorks.FDO;
 
 namespace LfMerge.Actions
 {
@@ -25,6 +26,7 @@ namespace LfMerge.Actions
 			IEnumerable<LfLexEntry> lexicon = GetLexiconForTesting(project, config);
 			foreach (LfLexEntry entry in lexicon)
 			{
+				Console.Write("{0}: ", entry.Guid);
 				if (entry.Lexeme != null && entry.Lexeme.Values != null)
 					Console.WriteLine(String.Join(", ", entry.Lexeme.Values.Select(x => (x.Value == null) ? "" : x.Value)));
 				foreach(LfSense sense in entry.Senses)
@@ -35,6 +37,59 @@ namespace LfMerge.Actions
 							Console.Write(" - " + sense.PartOfSpeech.Value);
 					}
 					Console.WriteLine();
+				}
+			}
+
+			if (project.FieldWorksProject == null)
+			{
+				Console.WriteLine("Failed to find the corresponding FieldWorks project!");
+				return;
+			}
+			FdoCache cache = project.FieldWorksProject.Cache;
+			if (cache == null)
+			{
+				Console.WriteLine("Failed to find the FDO cache!");
+				var fwProject = project.FieldWorksProject;
+				Console.WriteLine(fwProject.IsDisposed);
+				return;
+			}
+
+			IFdoServiceLocator servLoc = cache.ServiceLocator;
+			Console.WriteLine("Got the service locator");
+			ILexSenseRepository senseRepo = servLoc.GetInstance<ILexSenseRepository>();
+			Console.WriteLine("Got the sense repository");
+
+			lexicon = GetLexiconForTesting(project, config);
+			foreach (LfLexEntry entry in lexicon)
+			{
+				string entryName = String.Join(", ", entry.Lexeme.Values.Select(x => (x.Value == null) ? "" : x.Value));
+				Console.WriteLine("Checking entry {0} in lexicon", entryName);
+				if (entry.Senses == null) continue;
+				foreach(LfSense sense in entry.Senses)
+				{
+					if (sense.Definition == null) continue;
+					string senseName = String.Join(", ", sense.Definition.Values.Select(x => (x.Value == null) ? "" : x.Value));
+					Console.WriteLine("Checking sense {0} in entry {1} in lexicon", senseName, entryName);
+					ILexSense fwSense = senseRepo.GetObject(sense.LiftId);
+					if (fwSense == null)
+					{
+						//Console.WriteLine("Didn't find sense {0} in FDO", sense.LiftId);
+						continue;
+					}
+					IMultiString definition = fwSense.Definition;
+					if (definition == null)
+					{
+						//Console.WriteLine("Didn't find definition for {0} in FDO", sense.LiftId);
+						continue;
+					}
+
+					Console.WriteLine("Definition: {0}", definition.BestAnalysisVernacularAlternative.Text);
+					foreach (int wsid in definition.AvailableWritingSystemIds)
+					{
+						string wsid_str = servLoc.WritingSystemManager.GetStrFromWs(wsid);
+						var text = definition.get_String(wsid);
+						Console.WriteLine("{0}: {1}", wsid_str, text.Text);
+					}
 				}
 			}
 		}
