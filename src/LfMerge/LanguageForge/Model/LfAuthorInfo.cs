1ce92e0330ea1abc9d2860e127490eaf9f10e664
Robin Munn <robin_munn@sil.org>
2015-11-16 11:11:48 +0700

    Handle Mongo ObjectId references better
    ObjectId is a struct in the Mongo C# driver, so it's a non-nullable
    object by default. However, our PHP code stores null for these UserRef
    fields if no user reference is available. I had been making these fields
    strings, but nullable ObjectId values are better: they will round-trip
    correctly this way whether there is data there or not.

--- src/LfMerge/LanguageForge/Model/LfAuthorInfo.cs
+++ src/LfMerge/LanguageForge/Model/LfAuthorInfo.cs
@@ -1,14 +1,15 @@
 ﻿// Copyright (c) 2015 SIL International
 // This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
 using System;
+using MongoDB.Bson;
 
 namespace LfMerge.LanguageForge.Model
 {
 	public class LfAuthorInfo : LfFieldBase
 	{
-		public string CreatedByUserRef { get; set; }
+		public ObjectId? CreatedByUserRef { get; set; }
 		public DateTime CreatedDate { get; set; }
-		public string ModifiedByUserRef { get; set; }
+		public ObjectId? ModifiedByUserRef { get; set; }
 		public DateTime ModifiedDate { get; set; }
 	}
 }
