8379d084d880607c68ae035d0085e442af9bfa5f
Robin Munn <robin_munn@sil.org>
2015-12-15 15:56:15 +0700

    Add LfMergeSettingsIni class
    We will switch over to this class later; for now, it's not yet used.

---
+++ src/LfMerge/DefaultLfMergeSettings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LfMerge
+{
+	public static class DefaultLfMergeSettings
+	{
+		public const string DefaultIniText = @"
+BaseDir = ${HOME}/fwrepo/fw/DistFiles
+ReleaseDataDir = ReleaseData
+TemplatesDir = Templates
+MongoHostname = localhost
+MongoPort = 27017
+";
+	}
+}
+
