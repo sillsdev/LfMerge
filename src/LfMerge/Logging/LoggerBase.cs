ad6523672b3d300a20993a52bdeadd8aee4a6719
Robin Munn <robin_munn@sil.org>
2016-01-22 16:20:34 +0700

    Add ability to log to syslog

---
+++ src/LfMerge/Logging/LoggerBase.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) 2016 SIL International
+// This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
+using System;
+using System.Collections.Generic;
+
+namespace LfMerge.Logging
+{
+	public abstract class LoggerBase : ILogger
+	{
+		public LoggerBase()
+		{
+		}
+
+		// Log a single message, already formatted. See Emergency, Alert, etc. functions
+		// if you want to get functionst that act more like Console.WriteLine().
+		public abstract void Log(LogSeverity severity, string message);
+
+		// Log multiple messages at once, so that they will all end up in the log in one
+		// large block, without log messages from other programs interspersed. Most useful
+		// if you are collecting errors during a long-running process, and want to log all
+		// the errors at the end of the process.
+		public abstract void LogMany(LogSeverity severity, IEnumerable<string> messages);
+
+		// ***** Convenience functions for trivially replacing Console.WriteLine() *****
+
+		public void Emergency(string messageFormat, params object[] messageParts)
+		{
+			Log(LogSeverity.Emergency, String.Format(messageFormat, messageParts));
+		}
+
+		public void Alert(string messageFormat, params object[] messageParts)
+		{
+			Log(LogSeverity.Alert, String.Format(messageFormat, messageParts));
+		}
+
+		public void Critical(string messageFormat, params object[] messageParts)
+		{
+			Log(LogSeverity.Critical, String.Format(messageFormat, messageParts));
+		}
+
+		public void Error(string messageFormat, params object[] messageParts)
+		{
+			Log(LogSeverity.Error, String.Format(messageFormat, messageParts));
+		}
+
+		public void Warning(string messageFormat, params object[] messageParts)
+		{
+			Log(LogSeverity.Warning, String.Format(messageFormat, messageParts));
+		}
+
+		public void Notice(string messageFormat, params object[] messageParts)
+		{
+			Log(LogSeverity.Notice, String.Format(messageFormat, messageParts));
+		}
+
+		public void Info(string messageFormat, params object[] messageParts)
+		{
+			Log(LogSeverity.Info, String.Format(messageFormat, messageParts));
+		}
+
+		public void Debug(string messageFormat, params object[] messageParts)
+		{
+			Log(LogSeverity.Debug, String.Format(messageFormat, messageParts));
+		}
+	}
+}
+
---
+++ src/LfMerge/Logging/SyslogLogger.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) 2016 SIL International
+// This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
+using SIL.Linux.Logging;
+using System.Collections.Generic;
+
+namespace LfMerge.Logging
+{
+	public class SyslogLogger : LoggerBase
+	{
+		private SIL.Linux.Logging.SyslogLogger _logger;
+
+		public SyslogLogger()
+		{
+			_logger = new SIL.Linux.Logging.SyslogLogger();
+		}
+
+		private SyslogPriority SeverityToPriority(LogSeverity severity)
+		{
+			// If integer values of LogSeverity enum ever change, this function will need to be more complicated
+			int severityNum = (int)severity;
+			return (SyslogPriority)severityNum;
+		}
+
+		public override void LogMany(LogSeverity severity, IEnumerable<string> messages)
+		{
+			_logger.LogMany(SeverityToPriority(severity), messages);
+		}
+
+		public override void Log(LogSeverity severity, string message)
+		{
+			_logger.Log(SeverityToPriority(severity), message);
+		}
+	}
+}
+
