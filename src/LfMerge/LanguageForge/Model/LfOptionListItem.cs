9563e329ea67bb7e1f58797f59a169af5e02a0b3
Robin Munn <robin_munn@sil.org>
2016-02-10 14:11:52 +0700

    Fix binary GUIDs showing up in Mongo optionlists

--- src/LfMerge/LanguageForge/Model/LfOptionListItem.cs
+++ src/LfMerge/LanguageForge/Model/LfOptionListItem.cs
@@ -1,11 +1,14 @@
 ﻿// Copyright (c) 2016 SIL International
 // This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 using System;
 
 namespace LfMerge.LanguageForge.Model
 {
 	public class LfOptionListItem
 	{
+		[BsonRepresentation(BsonType.String)]
 		public Guid? Guid { get; set; }
 		public string Key { get; set; }
 		public string Value { get; set; }
