b8e16ab18027f69c1425e457853d07aa787cedf3
Robin Munn <robin_munn@sil.org>
2016-03-03 15:49:51 +0700

    Remove unused code from GrammarConverter
    It's practically a shell now; we can probably get rid of it completely.

--- src/LfMerge/DataConverters/GrammarConverter.cs
+++ src/LfMerge/DataConverters/GrammarConverter.cs
@@ -14,27 +14,5 @@ namespace LfMerge.DataConverters
 		public GrammarConverter(LfOptionList lfOptionList, int wsForKeys) : base(lfOptionList, wsForKeys, MagicStrings.LfOptionListCodeForGrammaticalInfo)
 		{
 		}
-
-		public override LfOptionList PrepareOptionListUpdate(ICmPossibilityList fdoOptionList)
-		{
-			return base.PrepareOptionListUpdate(fdoOptionList);
-		}
-
-		private string AbbrevHierarchyStringForWs(ICmPossibility poss, int wsId)
-		{
-			// The CmPossibility.AbbrevHierarchyString property uses the default analysis language.
-			// But we need to force a specific language (English) even if that is not the analysis language.
-			string ORC = "\ufffc";
-			ICmPossibility current = poss;
-			LinkedList<ICmPossibility> allAncestors = new LinkedList<ICmPossibility>();
-			while (current != null)
-			{
-				allAncestors.AddFirst(current);
-				current = current.Owner as ICmPossibility;
-			}
-			// TODO: The below line might fail if one of them doesn't have a corresponding string. Deal with that case.
-			return string.Join(ORC, allAncestors.Select(ancestor => ancestor.Abbreviation.get_String(wsId).Text));
-		}
-
 	}
 }
