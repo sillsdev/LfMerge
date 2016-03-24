db27041a7588382bbf73770bf6081a774beb7bcb
Eberhard Beilharz <eb1@sil.org>
2016-03-24 15:46:42 +0100

    Use flexbridge for send/receive

---
+++ src/LfMerge/Actions/Infrastructure/SendReceiveActionHandler.cs
@@ -0,0 +1,105 @@
+﻿// Copyright (c) 2016 SIL International
+// This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
+using System;
+using System.IO;
+using System.Reflection;
+using Autofac;
+using Chorus;
+using Chorus.sync;
+using Chorus.UI.Sync;
+using Chorus.VcsDrivers;
+using LfMerge.Settings;
+using LibFLExBridgeChorusPlugin.Infrastructure.ActionHandlers;
+using LibTriboroughBridgeChorusPlugin;
+using Palaso.Network;
+using Palaso.Progress;
+
+namespace LfMerge.Actions.Infrastructure
+{
+	public class SendReceiveActionHandler: SendReceiveAction
+	{
+		public SendReceiveActionHandler(LfMergeSettingsIni settings, LfMerge.Logging.ILogger logger,
+			IProgress progress, ILfProject project)
+		{
+			Settings = settings;
+			Logger = logger;
+			Progress = progress;
+			Project = project;
+		}
+
+		private LfMergeSettingsIni Settings { get; set; }
+		private LfMerge.Logging.ILogger Logger { get; set; }
+		private IProgress Progress { get; set; }
+		private ILfProject Project { get; set; }
+
+		protected override ChorusSystemSimple InitializeChorusSystem(string directoryName,
+			string user)
+		{
+			var chorusSystem = base.InitializeChorusSystem(directoryName, user);
+
+			var builder = new ContainerBuilder();
+			builder.Register<ProjectFolderConfiguration>(
+				c => new ProjectFolderConfiguration(ProjectFolderPath)).InstancePerLifetimeScope();
+			builder.Register<IProgress>(
+				c => Progress).InstancePerLifetimeScope();
+			builder.Update(chorusSystem.Container);
+
+			var assemblyName = Assembly.GetExecutingAssembly().GetName();
+			string applicationName = assemblyName.Name;
+			string applicationVersion = assemblyName.Version.ToString();
+
+			var controlModel = chorusSystem.Container.Resolve<SyncControlModelSimple>();
+			var syncOptions = controlModel.SyncOptions;
+			syncOptions.CheckinDescription = string.Format("[{0}: {1}] sync",
+				applicationName, applicationVersion);
+			var chorusHelper = MainClass.Container.Resolve<ChorusHelper>();
+			syncOptions.RepositorySourcesToTry.Add(
+				RepositoryAddress.Create("Language Depot", chorusHelper.GetSyncUri(Project)));
+
+			return chorusSystem;
+		}
+
+		private string ProjectFolderPath
+		{
+			get
+			{
+				return Path.Combine(Settings.WebWorkDirectory, Project.ProjectCode);
+			}
+		}
+
+		private SyncResults RunSync(ChorusSystemSimple chorusSystem)
+		{
+			var controlModel = chorusSystem.Container.Resolve<SyncControlModelSimple>();
+
+			var fwdataPathname = Path.Combine(ProjectFolderPath,
+				Project.ProjectCode + SharedConstants.FwXmlExtension);
+
+			var synchroniser = Synchronizer.FromProjectConfiguration(
+				chorusSystem.ProjectFolderConfiguration, Progress);
+			synchroniser.SynchronizerAdjunct = new LfMergeSychronizerAdjunct(fwdataPathname,
+				MagicStrings.FwFixitAppName, true); // Settings.VerboseProgress);
+
+			return synchroniser.SyncNow(controlModel.SyncOptions);
+		}
+
+		public void StartWorking()
+		{
+			// Syncing of a new repo is not currently supported.
+			// For implementation, look in ~/fwrepo/flexbridge/src/FLEx-ChorusPlugin/Infrastructure/ActionHandlers/SendReceiveActionHandler.cs
+			Logger.Notice("Syncing");
+
+			var result = Run(ProjectFolderPath, Project.LanguageDepotProject.Username, RunSync);
+
+			if (!result.Succeeded)
+			{
+				Logger.Error("Sync failed - {0}", result.ErrorEncountered);
+				return;
+			}
+			if (result.DidGetChangesFromOthers)
+				Logger.Notice("Received changes from others");
+			else
+				Logger.Notice("No changes from others");
+		}
+	}
+}
+
--- src/LfMerge/Actions/SynchronizeAction.cs
+++ src/LfMerge/Actions/SynchronizeAction.cs
@@ -1,22 +1,17 @@
 ﻿// Copyright (c) 2016 SIL International
 // This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
 using Autofac;
-using Chorus.sync;
-using Chorus.VcsDrivers;
 using LfMerge.Actions.Infrastructure;
 using LfMerge.Settings;
-using LibFLExBridgeChorusPlugin.Infrastructure;
-using LibTriboroughBridgeChorusPlugin;
-using Palaso.Progress;
-using System;
-using System.IO;
-using System.Reflection;
 
 namespace LfMerge.Actions
 {
 	public class SynchronizeAction: Action
 	{
-		public SynchronizeAction(LfMergeSettingsIni settings, LfMerge.Logging.ILogger logger) : base(settings, logger) {}
+		public SynchronizeAction(LfMergeSettingsIni settings, LfMerge.Logging.ILogger logger)
+			: base(settings, logger)
+		{
+		}
 
 		protected override ProcessingState.SendReceiveStates StateForCurrentAction
 		{
@@ -30,32 +25,7 @@ namespace LfMerge.Actions
 				GetAction(ActionNames.TransferMongoToFdo).Run(project);
 				LanguageForgeProject.DisposeProjectCache(project.ProjectCode);
 
-				// Syncing of a new repo is not currently supported.
-				// For implementation, look in ~/fwrepo/flexbridge/src/FLEx-ChorusPlugin/Infrastructure/ActionHandlers/SendReceiveActionHandler.cs
-				Logger.Notice("Syncing");
-				string applicationName = Assembly.GetExecutingAssembly().GetName().Name;
-				string applicationVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
-				var chorusHelper = MainClass.Container.Resolve<ChorusHelper>();
-				var repoPath = RepositoryAddress.Create("Language Depot", chorusHelper.GetSyncUri(project));
-				var syncOptions = new SyncOptions();
-				syncOptions.CheckinDescription = "[" + applicationName  + ": " + applicationVersion + "] sync";
-				syncOptions.RepositorySourcesToTry.Add(repoPath);
-				string projectFolderPath = Path.Combine(Settings.WebWorkDirectory, project.ProjectCode);
-				var projectConfig = new ProjectFolderConfiguration(projectFolderPath);
-				FlexFolderSystem.ConfigureChorusProjectFolder(projectConfig);
-				var synchroniser = Synchronizer.FromProjectConfiguration(projectConfig, Progress);
-				string fwdataFilePath = Path.Combine(projectFolderPath, project.ProjectCode + SharedConstants.FwXmlExtension);
-				synchroniser.SynchronizerAdjunct = new LfMergeSychronizerAdjunct(fwdataFilePath, MagicStrings.FwFixitAppName, true); // Settings.VerboseProgress);
-				SyncResults syncResult = synchroniser.SyncNow(syncOptions);
-				if (!syncResult.Succeeded)
-				{
-					Logger.Error("Sync failed - {0}", syncResult.ErrorEncountered);
-					return;
-				}
-				if (syncResult.DidGetChangesFromOthers)
-					Logger.Notice("Received changes from others");
-				else
-					Logger.Notice("No changes from others");
+				new SendReceiveActionHandler(Settings, Logger, Progress, project).StartWorking();
 
 				GetAction(ActionNames.TransferFdoToMongo).Run(project);
 			}
@@ -65,6 +35,6 @@ namespace LfMerge.Actions
 		{
 			get { return ActionNames.None; }
 		}
-	
+
 	}
 }
