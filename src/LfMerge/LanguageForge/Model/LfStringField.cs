e47ec4b476cfb85eb7d8c17fec85d4d5457c4d1a
Robin Munn <robin_munn@sil.org>
2016-03-31 15:44:01 +0700

    WIP to preserve GUIDs in custom MultiPara fields
    First attempt at preserving Guids in LfStringField. Appears to work, but
    unit tests fail if line 178 of ConvertMongoToFdoCustomField.cs is
    commented out (line 178 is the "no change in contents? skip" shortcut).
    So round-tripping really isn't working yet.

--- src/LfMerge/LanguageForge/Model/LfStringField.cs
+++ src/LfMerge/LanguageForge/Model/LfStringField.cs
@@ -2,14 +2,17 @@
 // This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
 using System;
 using System.Collections.Generic;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
 namespace LfMerge.LanguageForge.Model
 {
-	[BsonIgnoreExtraElements]
 	public class LfStringField : LfFieldBase
 	{
 		public string Value { get; set; }
+
+		[BsonRepresentation(BsonType.String)]  // Yes, this works with List<Guid>. Very nice.
+		public List<Guid> Guids { get; set; }
 
 		public bool IsEmpty { get { return String.IsNullOrEmpty(Value); } }
 
@@ -23,7 +26,12 @@ namespace LfMerge.LanguageForge.Model
 		{
 			if (source == null)
 				return null;
-			return new LfStringField { Value = source };
+			return new LfStringField { Value = source, Guids = new List<Guid>() };
+		}
+
+		public LfStringField()
+		{
+			Guids = new List<Guid>();
 		}
 
 		public Dictionary<string, string> AsDictionary()
