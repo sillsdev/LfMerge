89bc60df9c1661dd1a004378440c6f1c6e3f35e8
Robin Munn <robin_munn@sil.org>
2016-04-20 15:42:28 +0700

    Add files accidentally omitted from previous commit

---
+++ src/LfMerge/LanguageForge/Model/LfMultiParagraph.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) 2016 SIL International
+// This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
+using System;
+using System.Collections.Generic;
+
+namespace LfMerge.LanguageForge.Model
+{
+	public class LfMultiParagraph : LfFieldBase
+	{
+		public string Ws { get; set; }
+		public List<LfParagraph> Paras { get; set; }
+
+		public bool ShouldSerializeWs() { return !String.IsNullOrEmpty(Ws); }
+		public bool ShouldSerializeParas() { return (Paras != null && Paras.Count > 0); }
+
+		public LfMultiParagraph() {
+			Paras = new List<LfParagraph>();
+		}
+	}
+}
+
---
+++ src/LfMerge/LanguageForge/Model/LfParagraph.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2016 SIL International
+// This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
+using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LfMerge.LanguageForge.Model
+{
+	public class LfParagraph : LfFieldBase, IHasNullableGuid
+	{
+		[BsonRepresentation(BsonType.String)]
+		public Guid? Guid { get; set; }
+		public string StyleName { get; set; }
+		public string Contents { get; set; }
+
+		public bool ShouldSerializeGuid() { return (Guid != null && Guid.Value != System.Guid.Empty); }
+		public bool ShouldSerializeStyleName() { return !String.IsNullOrEmpty(StyleName); }
+		// Always serialize Contents even if empty. Thus, no ShouldSerializeContents() method needed.
+
+		public bool IsEmpty { get { return String.IsNullOrEmpty(Contents); } }
+	}
+}
+
