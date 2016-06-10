559a40d8bfe5552b7e80f9f1ae277cde2c756194
Eberhard Beilharz <eb1@sil.org>
2016-06-10 10:03:23 +0200

    Improve deploy script
    - Exit script if parameter is not specified
    - ask for sudo password

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,8 @@
 
 if [ $# -le 0 ];
 then
-    echo 'Please specify server type: either "build" or "production"'
+	echo 'Please specify server type: either "build" or "production"'
+	exit 1
 fi
 
-ansible-playbook -i hosts lfmerge.yaml --extra-vars "target=$1"
+ansible-playbook -i hosts lfmerge.yaml --extra-vars "target=$1" --ask-become-pass
