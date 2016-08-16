b0e56dce2cf611d4afedadf5a4631a86b717d8cb
Robin Munn <robin_munn@sil.org>
2016-08-16 16:31:20 +0700

    Helper functions for TsString property handling
    Thw switch() statement here is ugly, but because the enum in question
    had some overloaded values, we can't use ToString() since the C# spec
    makes no guarantee which enum's name it will return if two enums have
    the same name.

--- src/LfMerge/DataConverters/ConvertFdoToMongoTsStrings.cs
+++ src/LfMerge/DataConverters/ConvertFdoToMongoTsStrings.cs
@@ -35,6 +35,177 @@ namespace LfMerge.DataConverters
 			return tss.Text;
 		}
 
+		public static string TextFromTsString(ITsString tss)
+		{
+			// This will replace SafeTsStringText, and will actually deal with <span> elements
+
+			// Spec: the TsString properties ktptWs (an int property) and ktptNamedStyle (a string property)
+			// will be replaced with lang="(wsStr)" and class="styleName_PropValue". Other properties, that
+			// LF doesn't process, will be preserved in the class attribute as well, as follows:
+			//
+			// 1) Take value of string property, replace all space characters with "_SPACE_".
+			// 2) Take name of property and turn it into a string (sadly, enum's ToString() won't work here since there are multiple enums with the same int value).
+			// 3) Depending on whether it's an int or string prop, use "propi" or "props".
+			//
+			// Result: class="propi_1_ktptWs_1 props_1_ktptFontFamily_Times_SPACE_New_SPACE_Roman"
+			//
+			// TODO: Write that ordered list in a slightly more structured way now that I've got it all down on screen.
+		}
+
+		internal static string IntPropertyName(FwTextPropType prop)
+		{
+			// UGH. But we can't use prop.ToString() because the FwTextPropType enum was "overloaded"; that is, there are two
+			// different FwTextPropType values with the value 1 (FwTextPropType.ktptWs and FwTextPropType.ktptFontFamily) and
+			// the C# spec says you can't count on which one ToString() will return in that case. So we have to do this nonsense.
+			switch (prop)
+			{
+			case FwTextPropType.ktptWs:
+				return "ktptWs";
+			case FwTextPropType.ktptItalic:
+				return "ktptItalic";
+			case FwTextPropType.ktptBold:
+				return "ktptBold";
+			case FwTextPropType.ktptSuperscript:
+				return "ktptSuperscript";
+			case FwTextPropType.ktptUnderline:
+				return "ktptUnderline";
+			case FwTextPropType.ktptFontSize:
+				return "ktptFontSize";
+			case FwTextPropType.ktptOffset:
+				return "ktptOffset";
+			case FwTextPropType.ktptForeColor:
+				return "ktptForeColor";
+			case FwTextPropType.ktptBackColor:
+				return "ktptBackColor";
+			case FwTextPropType.ktptUnderColor:
+				return "ktptUnderColor";
+			case FwTextPropType.ktptBaseWs:
+				return "ktptBaseWs";
+			case FwTextPropType.ktptAlign:
+				return "ktptAlign";
+			case FwTextPropType.ktptFirstIndent:
+				return "ktptFirstIndent";
+			case FwTextPropType.ktptLeadingIndent:
+				return "ktptLeadingIndent";
+			case FwTextPropType.ktptMarginLeading:
+				return "ktptMarginLeading";
+			case FwTextPropType.ktptTrailingIndent:
+				return "ktptTrailingIndent";
+			case FwTextPropType.ktptMarginTrailing:
+				return "ktptMarginTrailing";
+			case FwTextPropType.ktptSpaceBefore:
+				return "ktptSpaceBefore";
+			case FwTextPropType.ktptMswMarginTop:
+				return "ktptMswMarginTop";
+			case FwTextPropType.ktptSpaceAfter:
+				return "ktptSpaceAfter";
+			case FwTextPropType.ktptMarginBottom:
+				return "ktptMarginBottom";
+			case FwTextPropType.ktptTabDef:
+				return "ktptTabDef";
+			case FwTextPropType.ktptLineHeight:
+				return "ktptLineHeight";
+			case FwTextPropType.ktptParaColor:
+				return "ktptParaColor";
+			case FwTextPropType.ktptSpellCheck:
+				return "ktptSpellCheck";
+			case FwTextPropType.ktptMarginTop:
+				return "ktptMarginTop";
+			case FwTextPropType.ktptRightToLeft:
+				return "ktptRightToLeft";
+			case FwTextPropType.ktptDirectionDepth:
+				return "ktptDirectionDepth";
+			case FwTextPropType.ktptPadLeading:
+				return "ktptPadLeading";
+			case FwTextPropType.ktptPadTrailing:
+				return "ktptPadTrailing";
+			case FwTextPropType.ktptPadTop:
+				return "ktptPadTop";
+			case FwTextPropType.ktptPadBottom:
+				return "ktptPadBottom";
+			case FwTextPropType.ktptBorderTop:
+				return "ktptBorderTop";
+			case FwTextPropType.ktptBorderBottom:
+				return "ktptBorderBottom";
+			case FwTextPropType.ktptBorderLeading:
+				return "ktptBorderLeading";
+			case FwTextPropType.ktptBorderTrailing:
+				return "ktptBorderTrailing";
+			case FwTextPropType.ktptBorderColor:
+				return "ktptBorderColor";
+			case FwTextPropType.ktptBulNumScheme:
+				return "ktptBulNumScheme";
+			case FwTextPropType.ktptBulNumStartAt:
+				return "ktptBulNumStartAt";
+			case FwTextPropType.ktptKeepWithNext:
+				return "ktptKeepWithNext";
+			case FwTextPropType.ktptKeepTogether:
+				return "ktptKeepTogether";
+			case FwTextPropType.ktptHyphenate:
+				return "ktptHyphenate";
+			case FwTextPropType.ktptMaxLines:
+				return "ktptMaxLines";
+			case FwTextPropType.ktptCellBorderWidth:
+				return "ktptCellBorderWidth";
+			case FwTextPropType.ktptCellSpacing:
+				return "ktptCellSpacing";
+			case FwTextPropType.ktptCellPadding:
+				return "ktptCellPadding";
+			case FwTextPropType.ktptEditable:
+				return "ktptEditable";
+			case FwTextPropType.ktptSetRowDefaults:
+				return "ktptSetRowDefaults";
+			case FwTextPropType.ktptRelLineHeight:
+				return "ktptRelLineHeight";
+			case FwTextPropType.ktptTableRule:
+				return "ktptTableRule";
+			case FwTextPropType.ktptWidowOrphanControl:
+				return "ktptWidowOrphanControl";
+			case FwTextPropType.ktptMarkItem:
+				return "ktptMarkItem";
+			default:
+				return "ktptUnknownIntProperty";
+			}
+		}
+
+		internal static string StringPropertyName(FwTextPropType prop)
+		{
+			// UGH. But we can't use prop.ToString() because the FwTextPropType enum was "overloaded"; that is, there are two
+			// different FwTextPropType values with the value 1 (FwTextPropType.ktptWs and FwTextPropType.ktptFontFamily) and
+			// the C# spec says you can't count on which one ToString() will return in that case. So we have to do this nonsense.
+			switch (prop)
+			{
+			case FwTextPropType.ktptFontFamily:
+				return "ktptFontFamily";
+			case FwTextPropType.ktptCharStyle:
+				return "ktptCharStyle";
+			case FwTextPropType.ktptParaStyle:
+				return "ktptParaStyle";
+			case FwTextPropType.ktptTabList:
+				return "ktptTabList";
+			case FwTextPropType.ktptTags:
+				return "ktptTags";
+			case FwTextPropType.ktptObjData:
+				return "ktptObjData";
+			case FwTextPropType.ktptFontVariations:
+				return "ktptFontVariations";
+			case FwTextPropType.ktptNamedStyle:
+				return "ktptNamedStyle";  // We handle this one specially, but keep it in the switch statement anyway
+			case FwTextPropType.ktptBulNumTxtBef:
+				return "ktptBulNumTxtBef";
+			case FwTextPropType.ktptBulNumTxtAft:
+				return "ktptBulNumTxtAft";
+			case FwTextPropType.ktptBulNumFontInfo:
+				return "ktptBulNumFontInfo";
+			case FwTextPropType.ktptWsStyle:
+				return "ktptWsStyle";
+			case FwTextPropType.ktptFieldName:
+				return "ktptFieldName";
+			default:
+				return "ktptUnknownStringProperty";
+			}
+		}
+
 		public string BestString(IMultiAccessorBase multiString)
 		{
 			// If this is an IMultiStringAccessor, we can just hand it off to GetBestAlternative
