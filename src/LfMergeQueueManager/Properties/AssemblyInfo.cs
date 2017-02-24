bde2a9a3887980ee254e63b62d182311645105eb
Eberhard Beilharz <eb1@sil.org>
2017-02-24 18:36:36 +0100

    Use GitVersion to set version number
    This makes the version numbers based on the git commits, allowing
    to see what commit a version corresponds to. This is helpful when
    seeing version numbers in log files or error messages.

--- src/LfMergeQueueManager/Properties/AssemblyInfo.cs
+++ src/LfMergeQueueManager/Properties/AssemblyInfo.cs
@@ -11,20 +11,8 @@ using CommandLine;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright (c) 2016 SIL International")]
+[assembly: AssemblyCopyright("Copyright (c) 2016-2017 SIL International")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyLicense("This software is licensed under the MIT License (http://opensource.org/licenses/MIT)")]
-
-// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
-// The form "{Major}.{Minor}.*" will automatically update the build and revision,
-// and "{Major}.{Minor}.{Build}.*" will update just the revision.
-
-[assembly: AssemblyVersion("1.0.*")]
-
-// The following attributes are used to specify the signing key for the assembly,
-// if desired. See the Mono documentation for more information about signing.
-
-//[assembly: AssemblyDelaySign(false)]
-//[assembly: AssemblyKeyFile("")]
 
