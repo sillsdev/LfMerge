88242850b390e90a016906b7e972480c2394b322
Eberhard Beilharz <eb1@sil.org>
2018-02-21 17:23:03 +0100

    Allow to compile with Mono 5
    Mono 5 uses the Roslyn compiler which doesn't define __MonoCS__.

--- src/LfMerge.Core/Logging/SyslogLogger.cs
+++ src/LfMerge.Core/Logging/SyslogLogger.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) 2016 SIL International
 // This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
 using System.Collections.Generic;
-#if __MonoCS__
+#if LINUX
 using SIL.Linux.Logging;
 #endif
 
@@ -9,18 +9,18 @@ namespace LfMerge.Core.Logging
 {
 	public class SyslogLogger : LoggerBase
 	{
-#if __MonoCS__
+#if LINUX
 		private SIL.Linux.Logging.SyslogLogger _logger;
 #endif
 
 		public SyslogLogger(string programName = null)
 		{
-#if __MonoCS__
+#if LINUX
 			_logger = new SIL.Linux.Logging.SyslogLogger(programName ?? "LfMerge");
 #endif
 		}
 
-#if __MonoCS__
+#if LINUX
 		private SyslogPriority SeverityToPriority(LogSeverity severity)
 		{
 			// If integer values of LogSeverity enum ever change, this function will need to be more complicated
@@ -31,14 +31,14 @@ namespace LfMerge.Core.Logging
 
 		public override void LogMany(LogSeverity severity, IEnumerable<string> messages)
 		{
-#if __MonoCS__
+#if LINUX
 			_logger.LogMany(SeverityToPriority(severity), messages);
 #endif
 		}
 
 		public override void Log(LogSeverity severity, string message)
 		{
-#if __MonoCS__
+#if LINUX
 			_logger.Log(SeverityToPriority(severity), message);
 #endif
 		}
