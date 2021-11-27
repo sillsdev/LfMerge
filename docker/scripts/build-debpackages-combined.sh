ed14320988abf6c1ff5fe7f3b12bfd1e24070f7a
Robin Munn <rmunn@pobox.com>
2021-11-28 01:52:45 +0700

    Temp debugging commit

--- docker/scripts/build-debpackages-combined.sh
+++ docker/scripts/build-debpackages-combined.sh
@@ -36,13 +36,6 @@ cd -
 	git clean -dxf --exclude=packages/ --exclude=lib/
 	git reset --hard
 
-	echo "AssemblyInfo files that would be updated if '$UPDATE_ASSEMBLYINFO_BY_SCRIPT' is not '' or '0':"
-	find src -name AssemblyInfo.cs -not -path FixFwData
-	if [ -n "$UPDATE_ASSEMBLYINFO_BY_SCRIPT" -a "$UPDATE_ASSEMBLYINFO_BY_SCRIPT" -ne 0 ]; then
-		echo "Will update AssemblyInfo.cs files"
-		find src -name AssemblyInfo.cs -not -path FixFwData -print0 | xargs -0 -n 1 ${HOME}/packages/lfmerge/docker/scripts/update-assemblyinfo.sh
-	fi
-
 	cat > NuGet.Config <<EOF
 <?xml version="1.0" encoding="utf-8"?>
 <configuration>
