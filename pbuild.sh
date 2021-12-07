072abb2c116e6d1ade5af98a728b00a3816896dc
Robin Munn <rmunn@pobox.com>
2021-12-07 11:02:53 +0700

    Local build now figures out FW8 and FW9 branches
    This isn't needed in the GitHub Actions workflow since that runs in
    separate containers with their own individually-checked-out copies of
    the repo, but in the local build we only have one checked-out branch,
    and we need to figure out the other branch to build. E.g. if the
    checked-out branch comes from a FW9 branch, then we need to calculate
    which FW8 branch to build (and default to fieldworks8-master). If the
    checked-out branch is a FW8 branch, then we need to figure out which FW9
    branch to build, and default to master.

--- pbuild.sh
+++ pbuild.sh
@@ -2,6 +2,44 @@
 
 which parallel >/dev/null || (echo 'Please run "sudo apt-get install parallel" and try again.'; exit 1)
 
+# Find appropriate branch(es) to build
+CURRENT_BRANCH="$(git name-rev --name-only HEAD)"
+PARENT_MAJOR_VERSION=$(git describe --long --match "v*" | cut -c1-2)
+
+# FW8 branches will have ancestors tagged v1.x, while FW9 branches will have ancestors tagged v2.x
+if [ "x$PARENT_MAJOR_VERSION" = "xv2" ]; then
+	IS_FW9=true
+else
+	IS_FW9=""
+fi
+
+if [ "${IS_FW9}" ]; then
+	echo Current branch is FW9, detecting FW8 branch to use...
+	FW9_BUILD_BRANCH="${CURRENT_BRANCH}"
+	if [ "$1" ]; then
+		FW8_BUILD_BRANCH="$1"
+	elif [ "${CURRENT_BRANCH}" = "master" -o "${CURRENT_BRANCH}" = "qa" -o "${CURRENT_BRANCH}" = "live" ]; then
+		FW8_BUILD_BRANCH="fieldworks8-${CURRENT_BRANCH}"
+	else
+		echo No FW 8 branch specified, assuming fieldworks8-master
+		echo To specify a different branch, run pbuild.sh '<'fw8-branch'>', e.g. '"'pbuild.sh feature/some-fw8-branch'"'
+		FW8_BUILD_BRANCH="fieldworks8-master"
+	fi
+else
+	echo Current branch is FW8, detecting FW9 branch to use...
+	FW8_BUILD_BRANCH="${CURRENT_BRANCH}"
+	if [ "$1" ]; then
+		FW9_BUILD_BRANCH="$1"
+	elif [ "${CURRENT_BRANCH}" = "fieldworks8-master" -o "${CURRENT_BRANCH}" = "fieldworks8-qa" -o "${CURRENT_BRANCH}" = "fieldworks8-live" ]; then
+		FW9_BUILD_BRANCH="${CURRENT_BRANCH##fieldworks8-}"
+	else
+		echo No FW 9 branch specified, assuming master
+		echo To specify a different branch, run pbuild.sh '<'fw8-branch'>', e.g. '"'pbuild.sh feature/some-fw8-branch'"'
+		FW9_BUILD_BRANCH="master"
+	fi
+fi
+echo Will build FW9 build from "${FW9_BUILD_BRANCH}" and FW8 builds from "${FW8_BUILD_BRANCH}"
+
 # Clean up any previous builds
 for f in 68 69 70 72; do
 # for f in 72; do
@@ -28,10 +66,10 @@ EOF
 
 # Run the build
 time parallel --no-notice <<EOF
-docker run --mount type=bind,source="$(pwd)",target=/home/builder/repo --mount type=tmpfs,dst=/tmp --env "BUILD_NUMBER=999" --env "DebPackageVersion=${DebPackageVersion}" --env "Version=${MsBuildVersion}" --env "MajorMinorPatch=${MajorMinorPatch}" --env "AssemblyVersion=${AssemblySemVer}" --env "FileVersion=${AssemblySemFileVer}" --env "InformationalVersion=${InformationalVersion}" --name tmp-lfmerge-build-7000068 lfmerge-build-7000068
-docker run --mount type=bind,source="$(pwd)",target=/home/builder/repo --mount type=tmpfs,dst=/tmp --env "BUILD_NUMBER=999" --env "DebPackageVersion=${DebPackageVersion}" --env "Version=${MsBuildVersion}" --env "MajorMinorPatch=${MajorMinorPatch}" --env "AssemblyVersion=${AssemblySemVer}" --env "FileVersion=${AssemblySemFileVer}" --env "InformationalVersion=${InformationalVersion}" --name tmp-lfmerge-build-7000069 lfmerge-build-7000069
-docker run --mount type=bind,source="$(pwd)",target=/home/builder/repo --mount type=tmpfs,dst=/tmp --env "BUILD_NUMBER=999" --env "DebPackageVersion=${DebPackageVersion}" --env "Version=${MsBuildVersion}" --env "MajorMinorPatch=${MajorMinorPatch}" --env "AssemblyVersion=${AssemblySemVer}" --env "FileVersion=${AssemblySemFileVer}" --env "InformationalVersion=${InformationalVersion}" --name tmp-lfmerge-build-7000070 lfmerge-build-7000070
-docker run --mount type=bind,source="$(pwd)",target=/home/builder/repo --mount type=tmpfs,dst=/tmp --env "BUILD_NUMBER=999" --env "DebPackageVersion=${DebPackageVersion}" --env "Version=${MsBuildVersion}" --env "MajorMinorPatch=${MajorMinorPatch}" --env "AssemblyVersion=${AssemblySemVer}" --env "FileVersion=${AssemblySemFileVer}" --env "InformationalVersion=${InformationalVersion}" --mount type=bind,src=/storage/nuget,dst=/storage/nuget --name tmp-lfmerge-build-7000072 lfmerge-build-7000072
+docker run --mount type=bind,source="$(pwd)",target=/home/builder/repo --mount type=tmpfs,dst=/tmp --env "BRANCH_TO_BUILD=${FW8_BUILD_BRANCH}" --env "BUILD_NUMBER=999" --env "DebPackageVersion=${DebPackageVersion}" --env "Version=${MsBuildVersion}" --env "MajorMinorPatch=${MajorMinorPatch}" --env "AssemblyVersion=${AssemblySemVer}" --env "FileVersion=${AssemblySemFileVer}" --env "InformationalVersion=${InformationalVersion}" --name tmp-lfmerge-build-7000068 lfmerge-build-7000068
+docker run --mount type=bind,source="$(pwd)",target=/home/builder/repo --mount type=tmpfs,dst=/tmp --env "BRANCH_TO_BUILD=${FW8_BUILD_BRANCH}" --env "BUILD_NUMBER=999" --env "DebPackageVersion=${DebPackageVersion}" --env "Version=${MsBuildVersion}" --env "MajorMinorPatch=${MajorMinorPatch}" --env "AssemblyVersion=${AssemblySemVer}" --env "FileVersion=${AssemblySemFileVer}" --env "InformationalVersion=${InformationalVersion}" --name tmp-lfmerge-build-7000069 lfmerge-build-7000069
+docker run --mount type=bind,source="$(pwd)",target=/home/builder/repo --mount type=tmpfs,dst=/tmp --env "BRANCH_TO_BUILD=${FW8_BUILD_BRANCH}" --env "BUILD_NUMBER=999" --env "DebPackageVersion=${DebPackageVersion}" --env "Version=${MsBuildVersion}" --env "MajorMinorPatch=${MajorMinorPatch}" --env "AssemblyVersion=${AssemblySemVer}" --env "FileVersion=${AssemblySemFileVer}" --env "InformationalVersion=${InformationalVersion}" --name tmp-lfmerge-build-7000070 lfmerge-build-7000070
+docker run --mount type=bind,source="$(pwd)",target=/home/builder/repo --mount type=tmpfs,dst=/tmp --env "BRANCH_TO_BUILD=${FW9_BUILD_BRANCH}" --env "BUILD_NUMBER=999" --env "DebPackageVersion=${DebPackageVersion}" --env "Version=${MsBuildVersion}" --env "MajorMinorPatch=${MajorMinorPatch}" --env "AssemblyVersion=${AssemblySemVer}" --env "FileVersion=${AssemblySemFileVer}" --env "InformationalVersion=${InformationalVersion}" --mount type=bind,src=/storage/nuget,dst=/storage/nuget --name tmp-lfmerge-build-7000072 lfmerge-build-7000072
 EOF
 
 # To run a single build instead, comment out the block above and uncomment the next line (and change 72 to 68/69/70 if needed)
