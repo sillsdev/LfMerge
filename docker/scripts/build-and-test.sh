700d0b77be8ec62f756cec601a61a7f4ca123da6
Robin Munn <rmunn@pobox.com>
2021-12-08 13:21:46 +0700

    No more pulling in PHP source in Dockerfile

--- docker/scripts/build-and-test.sh
+++ docker/scripts/build-and-test.sh
@@ -24,9 +24,6 @@ echo After setup-workspace.sh, pwd is $(pwd)
 "$SCRIPT_DIR"/compile-lfmerge-combined.sh ${DbVersion}
 
 if [ -n "$RUN_UNIT_TESTS" -a "$RUN_UNIT_TESTS" -ne 0 ]; then
-    rm -rf "$SCRIPT_DIR"/data/php
-    mkdir -p "$SCRIPT_DIR"/data/php
-    cp -a /var/www/html "$SCRIPT_DIR"/data/php/src
     "$SCRIPT_DIR"/test-lfmerge-combined.sh ${DbVersion}
 fi
 
