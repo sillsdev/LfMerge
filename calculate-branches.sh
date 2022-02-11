e0566df5398e85b5a350e2959a6132a272bc9499
Robin Munn <rmunn@pobox.com>
2022-02-11 11:55:57 +0700

    LfMerge build produces container instead of .deb files (#186)
    Instead of producing .deb files, the GHA workflow now produces a Docker
    container image, which will make it much easier to include lfmerge as a
    separate container in the web-languageforge build.

---
+++ calculate-branches.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Find appropriate branch(es) to build
+CURRENT_BRANCH=${GITHUB_REF:-$(git name-rev --name-only HEAD)}
+PARENT_MAJOR_VERSION=$(git describe --long --match "v*" | cut -c1-2)
+
+# FW8 branches will have ancestors tagged v1.x, while FW9 branches will have ancestors tagged v2.x
+if [ "x$PARENT_MAJOR_VERSION" = "xv2" ]; then
+	IS_FW9=true
+else
+	IS_FW9=""
+fi
+
+if [ "${IS_FW9}" ]; then
+	FW9_BUILD_BRANCH="${CURRENT_BRANCH}"
+	if [ "$1" ]; then
+		FW8_BUILD_BRANCH="$1"
+	elif [ "${CURRENT_BRANCH}" = "master" -o "${CURRENT_BRANCH}" = "qa" -o "${CURRENT_BRANCH}" = "live" ]; then
+		FW8_BUILD_BRANCH="fieldworks8-${CURRENT_BRANCH}"
+	else
+		FW8_BUILD_BRANCH="fieldworks8-master"
+	fi
+else
+	FW8_BUILD_BRANCH="${CURRENT_BRANCH}"
+	if [ "$1" ]; then
+		FW9_BUILD_BRANCH="$1"
+	elif [ "${CURRENT_BRANCH}" = "fieldworks8-master" -o "${CURRENT_BRANCH}" = "fieldworks8-qa" -o "${CURRENT_BRANCH}" = "fieldworks8-live" ]; then
+		FW9_BUILD_BRANCH="${CURRENT_BRANCH##fieldworks8-}"
+	else
+		FW9_BUILD_BRANCH="master"
+	fi
+fi
+
+echo FW8Branch="${FW8_BUILD_BRANCH}"
+echo FW9Branch="${FW9_BUILD_BRANCH}"
