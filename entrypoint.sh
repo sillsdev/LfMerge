43eb358c38e1ee4e897ec06659fd3e43aa94961f
Robin Munn <rmunn@pobox.com>
2022-06-10 12:56:29 +0700

    Exit LfMerge container faster by handling SIGTERM (#274)
    * Handle SIGTERM in entrypoint scripts
    
    This will allow the container to shut down cleanly in docker-compose and
    Kubernetes, rather than getting SIGKILL when it doesn't answer SIGTERM.
    
    * Ensure lfmerge container will always log to stdout
    
    In some cases, the `logger` command used to ensure that /var/log/syslog
    has at least one message in it was completing before syslog was actually
    created, and so the `tail -f` command failed. We now add a loop that
    will wait until syslog exists, then run `tail -f`. That way the LfMerge
    logs will always show up on stdout for use in `kubectl logs -f`.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+# Catch SIGTERM and exit cleanly
+trap "exit" TERM
+
 # rsyslog needs to run so that lfmerge can log to /var/log/syslog
 /etc/init.d/rsyslog start
 
 # Ensure /var/log/syslog exists so tail -f /var/log/syslog will run
 logger "Starting container..."
 # echo /var/log/syslog to container stdout so it shows up in `kubectl logs`
+# First waiting for syslog to exist (race condition: `logger` can return before syslog file is created)
+until [ -r /var/log/syslog ]; do
+  sleep 0.2
+done
 tail -f /var/log/syslog &
 
 # run lfmergeqm on startup to clear out any failed send/receive sessions from previous container
