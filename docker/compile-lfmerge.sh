ce9a70b736f8dd250db46ece2e6dd565e7af0a2a
Joseph Myers <wjosephmyers@gmail.com>
2022-06-27 16:15:26 +0700

    Replacing usage of mono with dotnet and using its nuget
    Couple notes:
    Removed RestoreBuildTasks. It doesn't appear to be necessary -- Nuget and the relevant projects should handle this if it turns out it is.
    Didn't replace --debug flag in mono with anything. It's unclear if the theoretical dotnet equivalent would perform as desired, but we could maybe use "verbosity" if needed.
    
    These changes are experimental, but a commit is required to test, since in docker creation pending changes are cleared.

--- docker/compile-lfmerge.sh
+++ docker/compile-lfmerge.sh
@@ -2,12 +2,9 @@
 echo "Compiling LfMerge and running unit tests"
 BUILD=Release . environ
 
-echo "Using $(which mono)"
-export FrameworkPathOverride=/opt/mono5-sil/lib/mono/4.5
+echo "Using $(which dotnet)"
 dotnet build /t:CompileOnly /v:detailed /property:Configuration=Release build/LfMerge.proj
 # TODO: Determine if this symlink is necessary
 ln -sf ../Mercurial output/
-cd /build/LfMerge/output/Release/net462
-export PATH="${PATH}:/opt/mono5-sil/bin"
 # TODO: Test all DLLs
 # dotnet test -f net462 LfMerge.Core.Tests.dll
