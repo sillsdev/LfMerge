38c7d6bc14b0478bdf2fb42c883ee5e27da57576
Robin Munn <rmunn@pobox.com>
2022-06-29 15:18:57 +0700

    Change SyslogLogger to log to stdout

--- src/LfMerge.Core/Logging/SyslogLogger.cs
+++ src/LfMerge.Core/Logging/SyslogLogger.cs
@@ -2,52 +2,23 @@
 // This software is licensed under the MIT license (http://opensource.org/licenses/MIT)
 using System;
 using System.Collections.Generic;
-using SIL.Linux.Logging;
-using SIL.PlatformUtilities;
 
 namespace LfMerge.Core.Logging
 {
 	public class SyslogLogger : LoggerBase
 	{
-		private SIL.Linux.Logging.SyslogLogger _logger;
-
 		public SyslogLogger(string programName = null)
 		{
-			if (Platform.IsLinux)
-			{
-				_logger = new SIL.Linux.Logging.SyslogLogger(programName ?? "LfMerge");
-			}
-		}
-
-		private SyslogPriority SeverityToPriority(LogSeverity severity)
-		{
-			// If integer values of LogSeverity enum ever change, this function will need to be more complicated
-			int severityNum = (int)severity;
-			return (SyslogPriority)severityNum;
 		}
 
 		public override void LogMany(LogSeverity severity, IEnumerable<string> messages)
 		{
-			if (_logger == null)
-			{
-				Console.WriteLine(string.Join(Environment.NewLine, messages));
-			}
-			else
-			{
-				_logger.LogMany(SeverityToPriority(severity), messages);
-			}
+			Console.WriteLine(string.Join(Environment.NewLine, messages));
 		}
 
 		public override void Log(LogSeverity severity, string message)
 		{
-			if (_logger == null)
-			{
-				Console.WriteLine(message);
-			}
-			else
-			{
-				_logger.Log(SeverityToPriority(severity), message);
-			}
+			Console.WriteLine(message);
 		}
 	}
 }
