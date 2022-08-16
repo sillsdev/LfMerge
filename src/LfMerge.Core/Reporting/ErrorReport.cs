669429c0f4955654b3654733d6d51a9a1791d22f
Robin Munn <rmunn@pobox.com>
2022-08-16 11:27:18 +0700

    Whitespace fix: tabs instead of spaces
    Two files got created with tabs instead of spaces. Fixing that now.

--- src/LfMerge.Core/Reporting/ErrorReport.cs
+++ src/LfMerge.Core/Reporting/ErrorReport.cs
@@ -9,34 +9,34 @@ using SIL.LCModel;
 
 namespace LfMerge.Core.Reporting
 {
-    public class ErrorReport
-    {
-        public DateTime Timestamp { get; set; }
+	public class ErrorReport
+	{
+		public DateTime Timestamp { get; set; }
 
-        public string ExceptionMessage { get; set; }
+		public string ExceptionMessage { get; set; }
 
-        public ConversionErrorReport Skipped { get; set; }
+		public ConversionErrorReport Skipped { get; set; }
 
-        public static ErrorReport Create(ConversionErrorReport skipped, Exception overallException)
-        {
-            return new ErrorReport {
-                Timestamp = DateTime.UtcNow,
-                ExceptionMessage = overallException?.ToString(),
-                Skipped = skipped
-            };
-        }
+		public static ErrorReport Create(ConversionErrorReport skipped, Exception overallException)
+		{
+			return new ErrorReport {
+				Timestamp = DateTime.UtcNow,
+				ExceptionMessage = overallException?.ToString(),
+				Skipped = skipped
+			};
+		}
 
-        public ErrorReport WithSkipped(ConversionErrorReport skipped)
-        {
-            return new ErrorReport {
-                Timestamp = DateTime.UtcNow,
-                ExceptionMessage = ExceptionMessage,
-                Skipped = skipped
-            };
-        }
+		public ErrorReport WithSkipped(ConversionErrorReport skipped)
+		{
+			return new ErrorReport {
+				Timestamp = DateTime.UtcNow,
+				ExceptionMessage = ExceptionMessage,
+				Skipped = skipped
+			};
+		}
 
-        public static ErrorReport CreateOrUpdate(ErrorReport orig, ConversionErrorReport skipped)
-        {
+		public static ErrorReport CreateOrUpdate(ErrorReport orig, ConversionErrorReport skipped)
+		{
 			if (orig == null) {
 				return new ErrorReport {
 					Timestamp = DateTime.UtcNow,
@@ -50,70 +50,70 @@ namespace LfMerge.Core.Reporting
 					Skipped = skipped
 				};
 			}
-        }
+		}
 
-        public static ErrorReport WithMongoErrors(ErrorReport orig, ConversionError<ILexEntry> toMongoErrors)
-        {
+		public static ErrorReport WithMongoErrors(ErrorReport orig, ConversionError<ILexEntry> toMongoErrors)
+		{
 			var skipped = orig?.Skipped ?? ConversionErrorReport.FromMongoConversionError(toMongoErrors);
 			return ErrorReport.CreateOrUpdate(orig, skipped);
-        }
+		}
 
-        public static ErrorReport WithLcmErrors(ErrorReport orig, ConversionError<LfLexEntry> toLcmErrors)
-        {
+		public static ErrorReport WithLcmErrors(ErrorReport orig, ConversionError<LfLexEntry> toLcmErrors)
+		{
 			var skipped = orig?.Skipped ?? ConversionErrorReport.FromLcmConversionError(toLcmErrors);
 			return ErrorReport.CreateOrUpdate(orig, skipped);
-        }
-    }
+		}
+	}
 
-    public class ConversionErrorReport
-    {
-        public ConversionErrors ToMongo { get; set; }
-        public ConversionErrors ToLcm { get; set; }
+	public class ConversionErrorReport
+	{
+		public ConversionErrors ToMongo { get; set; }
+		public ConversionErrors ToLcm { get; set; }
 
-        public static ConversionErrorReport Create(ConversionErrors toMongoErrors, ConversionErrors toLcmErrors)
-        {
-            return new ConversionErrorReport {
-                ToMongo = toMongoErrors,
-                ToLcm = toLcmErrors
-            };
-        }
+		public static ConversionErrorReport Create(ConversionErrors toMongoErrors, ConversionErrors toLcmErrors)
+		{
+			return new ConversionErrorReport {
+				ToMongo = toMongoErrors,
+				ToLcm = toLcmErrors
+			};
+		}
 
-        public static ConversionErrorReport FromMongoConversionError<T>(ConversionError<T> mongoErrors)
-        {
-            return new ConversionErrorReport {
-                ToMongo = ConversionErrors.FromConversionError(mongoErrors),
+		public static ConversionErrorReport FromMongoConversionError<T>(ConversionError<T> mongoErrors)
+		{
+			return new ConversionErrorReport {
+				ToMongo = ConversionErrors.FromConversionError(mongoErrors),
 				ToLcm = null,
-            };
-        }
+			};
+		}
 
-        public static ConversionErrorReport FromLcmConversionError<T>(ConversionError<T> lcmErrors)
-        {
-            return new ConversionErrorReport {
-                ToMongo = null,
+		public static ConversionErrorReport FromLcmConversionError<T>(ConversionError<T> lcmErrors)
+		{
+			return new ConversionErrorReport {
+				ToMongo = null,
 				ToLcm = ConversionErrors.FromConversionError(lcmErrors),
-            };
-        }
-    }
+			};
+		}
+	}
 
-    public class ConversionErrors
-    {
-        public ErrorList Entries { get; set; }
-        public ErrorList Comments { get; set; }
+	public class ConversionErrors
+	{
+		public ErrorList Entries { get; set; }
+		public ErrorList Comments { get; set; }
 
-        public static ConversionErrors FromConversionError<T>(
+		public static ConversionErrors FromConversionError<T>(
 			ConversionError<T> errorSet)
-        {
-            return new ConversionErrors {
-                Entries = ErrorList.FromEntryErrors<T>(errorSet.EntryErrors),
+		{
+			return new ConversionErrors {
+				Entries = ErrorList.FromEntryErrors<T>(errorSet.EntryErrors),
 				Comments = ErrorList.FromCommentErrors<T>(errorSet.CommentErrors),
-            };
-        }
-    }
+			};
+		}
+	}
 
-    public class ErrorList
-    {
-        public int Count { get { return List?.Count ?? 0; } }
-        public IList<SingleItemReport> List { get; set; }
+	public class ErrorList
+	{
+		public int Count { get { return List?.Count ?? 0; } }
+		public IList<SingleItemReport> List { get; set; }
 
 		public static ErrorList FromEntryErrors<T>(List<EntryConversionError<T>> entryErrors)
 		{
@@ -128,17 +128,17 @@ namespace LfMerge.Core.Reporting
 				List = commentErrors.Select(SingleItemReport.FromCommentError<T>).ToList()
 			};
 		}
-    }
+	}
 
 	public class SingleItemReport
 	{
-        public Guid Guid { get; set; }
-        public string Id { get; set; }
-        public string ExceptionMessage { get; set; }
-        public string Label { get; set; }
-        public SingleItemReport Entry { get; set; }
+		public Guid Guid { get; set; }
+		public string Id { get; set; }
+		public string ExceptionMessage { get; set; }
+		public string Label { get; set; }
+		public SingleItemReport Entry { get; set; }
 
-        public bool ShouldSerializeEntry() { return Entry != null; }
+		public bool ShouldSerializeEntry() { return Entry != null; }
 
 		public static SingleItemReport FromEntryError<T>(EntryConversionError<T> error)
 		{
