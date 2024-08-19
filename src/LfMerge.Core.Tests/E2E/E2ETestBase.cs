aaf4f36886e621889b5b36224d012d4a54ee7e2e
Robin Munn <rmunn@pobox.com>
2024-08-19 12:01:01 +0700

    Clean up now-unused code in E2ETestBase

--- src/LfMerge.Core.Tests/E2E/E2ETestBase.cs
+++ src/LfMerge.Core.Tests/E2E/E2ETestBase.cs
@@ -23,49 +23,13 @@ namespace LfMerge.Core.Tests.E2E
 	[Category("IntegrationTests")]
 	public class E2ETestBase : SRTestBase
 	{
-		public LfMergeSettings _lDSettings;
-		public TemporaryFolder _languageDepotFolder;
-		public LanguageForgeProject _lfProject;
-		public SynchronizeAction _synchronizeAction;
 		public MongoConnectionDouble _mongoConnection;
 		public MongoProjectRecordFactory _recordFactory;
-		public string _workDir;
-		public const string TestLangProj = "testlangproj";
-		public const string TestLangProjModified = "testlangproj-modified";
-
-		public void LcmSendReceive(FwProject project, string code, string? localCode = null, string? commitMsg = null)
-		{
-			// LfMergeBridge.LfMergeBridge.Execute??
-		}
-
 
 		[SetUp]
 		public void Setup()
 		{
 			MagicStrings.SetMinimalModelVersion(LcmCache.ModelVersion);
-
-			// SyncActionTests used to create mock LD server -- not needed since we use real (local) LexBox here
-
-			// _languageDepotFolder = new TemporaryFolder(TestContext.CurrentContext.Test.Name + Path.GetRandomFileName());
-			// _lDSettings = new LfMergeSettingsDouble(_languageDepotFolder.Path);
-			// Directory.CreateDirectory(_lDSettings.WebWorkDirectory);
-			// LanguageDepotMock.ProjectFolderPath =
-			// 	Path.Combine(_lDSettings.WebWorkDirectory, TestLangProj);
-			// Directory.CreateDirectory(LanguageDepotMock.ProjectFolderPath);
-			// LanguageDepotMock.Server = new MercurialServer(LanguageDepotMock.ProjectFolderPath);
-
-			// SyncActionTests used to create local LF project from the embedded test data -- here we will let each test do that
-
-			// _lfProject = LanguageForgeProject.Create(TestLangProj);
-
-			// TODO: get far enough to need to test this
-			_synchronizeAction = new SynchronizeAction(TestEnv.Settings, TestEnv.Logger);
-
-			// SyncActionTests used to set the current working directory so that Mercurial could be found; no longer needed now
-
-			// _workDir = Directory.GetCurrentDirectory();
-			// Directory.SetCurrentDirectory(ExecutionEnvironment.DirectoryOfExecutingAssembly);
-
 			_mongoConnection = MainClass.Container.Resolve<IMongoConnection>() as MongoConnectionDouble;
 			if (_mongoConnection == null)
 				throw new AssertionException("E2E tests need a mock MongoConnection that stores data in order to work.");
